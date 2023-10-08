11384be07283f3cf09205a5ef1996d420ea62ba5
HopeLotriet <hopelotriet@gmail.com>
2023-10-08 14:24:20 +0200

    Added the Guidelines

--- Flask/model.py
+++ Flask/model.py
@@ -23,7 +23,9 @@ app = Flask(__name__)
 def search():
     return render_template("search.html")
 
-
+@app.route("/classification")
+def classification():
+    return render_template("classification.html")
 
 # Get the absolute path to the model file
 model_file_path = os.path.abspath('Best_RFModel.pkl')
@@ -34,7 +36,7 @@ model = joblib.load(model_file_path)
 
 
 @app.route('/predict', methods=['POST'])
-def predict():
+def check_rule():
     # Get the input data from the form
     input_features = request.form.to_dict()
     
@@ -49,6 +51,37 @@ def predict():
     
     # Render the result in the HTML file
     return jsonify({'prediction': result})
+
+@app.route('/classify', methods=['POST'])
+def classify_drug():
+    # Get the input data from the form
+    input_features = request.form.to_dict()
+    
+    # Convert input features to a DataFrame
+    data = pd.DataFrame([input_features])
+    
+    # Make predictions
+    prediction = model.predict(data)
+    
+    # Determine if the molecular properties pass the Lipinski Rule
+    properties_passed = sum(prediction)
+    total_properties = len(prediction)
+    pass_percentage = (properties_passed / total_properties) * 100
+    
+    # Classify the drug based on pass percentage
+    if pass_percentage >= 90: 
+        classification = "Excellent Drug"
+    elif pass_percentage >= 70: 
+        classification = "Good Drug"
+    elif pass_percentage >= 50: 
+        classification = "Moderate Drug"
+    elif pass_percentage >= 20: 
+        classification = "Bad Drug"
+    else: 
+        classification = "Very Bad Drug"
+    
+    # Return the prediction result and classification as JSON
+    return jsonify({'pass_percentage': pass_percentage, 'classification': classification})
 
 if __name__ == '__main__':
     app.run(debug=True)
